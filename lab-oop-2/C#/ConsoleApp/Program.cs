dcbf3a36546841fb82cc8565c5112858124cff0d
DedKymus228 <sbaskatov5@gmail.com>
2026-02-12 20:14:21 +0200

    complete C# lab 2

---
+++ lab-oop-2/C#/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyGeometryLib;
+
+namespace Lab2
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+        Triangle exampleTriangle = new Triangle();
+
+        Triangle exampleTriangle2 = new Triangle(10, 1, 1, 1, 1, 1);
+
+        Triangle exampleTriangle3 = new Triangle(exampleTriangle2);
+        
+
+
+        Console.WriteLine("Coordinates: " + exampleTriangle2.GetCoordinates());
+        Console.WriteLine("Perimetr: " + exampleTriangle2.GetPerimeter());
+        Console.WriteLine("Area: " + exampleTriangle2.GetArea());
+
+        }
+    }
+}
---
+++ lab-oop-2/C#/MyGeometryLIb/Triangle.cs
@@ -0,0 +1,55 @@
+﻿namespace MyGeometryLib;
+
+public class Triangle
+{
+    
+    private double x1, y1, x2, y2, x3, y3;
+    private double GetSide(double ax, double ay, double bx, double by)
+    { 
+        return Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2));
+    }
+
+    public Triangle()
+    {
+        x1 = 0.0;
+        y1 = 0.0;
+        x2 = 0.0;
+        y2 = 0.0;
+        x3 = 0.0;
+        y3 = 0.0;
+    }
+
+    public Triangle(double X1, double Y1, double X2, double Y2, double X3, double Y3)
+    {
+        x1 = X1;
+        y1 = Y1;
+        x2 = X2;
+        y2 = Y2;
+        x3 = X3;
+        y3 = Y3;
+        
+    }
+
+    public Triangle(Triangle other){}
+    
+    ~Triangle() {}
+
+    public string GetCoordinates()
+    {
+        return $"A({x1},{y1}), B({x2},{y2}), C({x3},{y3})";
+    }
+
+    public double GetArea()
+    {
+        return 0.5 * Math.Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+    }
+
+    public double GetPerimeter()
+    {
+        double a = GetSide(x1, y1, x2, y2);
+        double b = GetSide(x2, y2, x3, y3);
+        double c = GetSide(x3, y3, x1, y1);
+        return a + b + c;
+    }
+        
+}
